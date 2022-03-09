8649a29e95f260f7e8699f05549d67f21f333ce6
Yunior Espinal <yunior.espinal@me.com>
2022-03-09 14:43:48 -0500

    Update genkey.py

--- {{cookiecutter.project_slug}}/apps/core/management/commands/genkey.py
+++ {{cookiecutter.project_slug}}/apps/core/management/commands/genkey.py
@@ -5,7 +5,7 @@ from django.core.management import BaseCommand
 
 
 def gen_key():
-    """Generates a 32 bit hex key."""
+    """Generates a 32-bit hex key."""
     for x in range(1):
         key = secrets.token_hex(32)
         sys.stdout.write('Key ' + str(x+1) + ': ' + key + "\n")
